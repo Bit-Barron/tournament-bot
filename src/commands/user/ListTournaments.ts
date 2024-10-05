48b4d6c1f39c10a21987a8edc32c80416c85fec8
bit-barron <bit.barron22@gmail.com>
2024-10-06 00:57:22 +0200

    sdf

--- src/commands/user/ListTournaments.ts
+++ src/commands/user/ListTournaments.ts
@@ -25,20 +25,9 @@ export class ListTournaments {
         return;
       }
 
-      const embed = new EmbedBuilder()
-        .setColor("#0099ff")
-        .setTitle("Tournament List")
-        .setDescription("Here are all the available tournaments:")
-        .addFields(
-          tournaments.map((t: { tournament_name: any; id: any; }) => ({
-            name: t.tournament_name,
-            value: `ID: ${t.id}`,
-            inline: true,
-          }))
-        )
-        .setTimestamp();
+      const embeds = this.createTournamentEmbeds(tournaments);
 
-      await interaction.editReply({ embeds: [embed] });
+      await interaction.editReply({ embeds });
     } catch (error) {
       console.error("Error listing tournaments:", error);
       await interaction.editReply({
@@ -46,5 +35,30 @@ export class ListTournaments {
           "An error occurred while fetching the tournament list. Please try again later.",
       });
     }
+  }
+
+  private createTournamentEmbeds(tournaments: any[]): EmbedBuilder[] {
+    const embeds: EmbedBuilder[] = [];
+    const tournamentsPerEmbed = 25;
+
+    for (let i = 0; i < tournaments.length; i += tournamentsPerEmbed) {
+      const tournamentChunk = tournaments.slice(i, i + tournamentsPerEmbed);
+      const embed = new EmbedBuilder()
+        .setColor("#0099ff")
+        .setTitle(`Tournament List (${i / tournamentsPerEmbed + 1})`)
+        .setDescription("Here are the available tournaments:")
+        .addFields(
+          tournamentChunk.map((t) => ({
+            name: t.tournament_name,
+            value: `ID: ${t.id}`,
+            inline: true,
+          }))
+        )
+        .setTimestamp();
+
+      embeds.push(embed);
+    }
+
+    return embeds;
   }
 }
