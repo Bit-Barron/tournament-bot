8f94397898fbde749e6bfef95325b3ec74f38b30
bit-barron <bit.barron22@gmail.com>
2024-10-05 04:14:40 +0200

    clean up

---
+++ src/commands/admin/CancelTournament.ts
@@ -0,0 +1,49 @@
+import { Discord, Slash, SlashOption } from "discordx";
+import { ApplicationCommandOptionType, CommandInteraction } from "discord.js";
+import prisma from "../../lib/prisma.js";
+
+@Discord()
+export class CancelTournament {
+  @Slash({
+    name: "cancel",
+    description: "Cancel a tournament",
+  })
+  async cancelTournament(
+    @SlashOption({
+      name: "tournament_id",
+      description: "The ID of the tournament to cancel",
+      type: ApplicationCommandOptionType.String,
+      required: true,
+    })
+    tournamentId: string,
+
+    interaction: CommandInteraction
+  ) {
+    try {
+      const tournament = await prisma.tournament.findUnique({
+        where: {
+          id: parseInt(tournamentId),
+        },
+      });
+
+      if (!tournament) {
+        throw new Error("Tournament not found");
+      }
+
+      await prisma.tournament.update({
+        where: {
+          id: parseInt(tournamentId),
+        },
+        data: {
+          status: "CANCELLED",
+        },
+      });
+
+      await interaction.reply(
+        `Tournament cancelled: ${tournament.tournament_name}`
+      );
+    } catch (error) {
+      await interaction.reply(`Error cancelling tournament: ${error}`);
+    }
+  }
+}
