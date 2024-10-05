da9bb7c0e567b4c6ac0781c50c4e7c6214dce51f
bit-barron <bit.barron22@gmail.com>
2024-10-05 18:54:25 +0200

    clean up

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,5 +1,3 @@
-import { CommandInteraction, GuildMember } from "discord.js";
-
 export function getStatusColor(status: string): number {
   switch (status) {
     case "PENDING":
@@ -13,4 +11,14 @@ export function getStatusColor(status: string): number {
     default:
       return 0x808080;
   }
+}
+
+export function calculateDuration(startDate: Date): string {
+  const endDate = new Date();
+  const durationMs = endDate.getTime() - startDate.getTime();
+  const days = Math.floor(durationMs / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (durationMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  return `${days} days, ${hours} hours`;
 }
